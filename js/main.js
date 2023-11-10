c886bbc2b1f9a3e689fee146188336fb82a6b7bd
Mohammad riazzuddin <c3022740@hallam.shu.ac.uk>
2023-11-10 10:53:43 +0000

    commit

--- js/main.js
+++ js/main.js
@@ -6,6 +6,7 @@
     //code started
 document.getElementById("myHeading").innerHTML = "Yusuf Rick";
 document.querySelector('nav ul li').setAttribute('class','currentPage');
+document.querySelector('nav ul li');
 
 //code ends
 
