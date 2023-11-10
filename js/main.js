30e3bd62c231f85747cd3746d5af5b469888398f
Mohammad riazzuddin <c3022740@hallam.shu.ac.uk>
2023-11-10 10:30:43 +0000

    commit

---
+++ js/main.js
@@ -0,0 +1,5 @@
+(function (){
+    // code starts
+    document.getElementById("myHeading").innerHTML = "Yusuf Rick";
+    // code ends
+})();
--- script.js
+++
@@ -1,7 +0,0 @@
-
-
-alert("Sup worlddd");
-(function init()
-{
-    console.info(document.getElementById("myHeading").innerHTML = "Yusuf Rick");
-})();
